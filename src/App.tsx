716f46f01f34be7dcd6eb54eb2fb1d5d569c3497
Alex-Nicalace <anicalace@gmail.com>
2025-05-08 18:13:06 +0300

    Checkbox: create

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Checkbox from './components/ui/Checkbox';
+
+function App() {
+  return (
+    <>
+      <Checkbox label="Enabled" />
+      <Checkbox label="Auto-refresh every 5 seconds" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,17 @@
+import type { InputHTMLAttributes } from 'react';
+import classes from './Checkbox.module.css';
+
+type TCheckboxProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
+  label?: string;
+};
+function Checkbox({ label, className, ...props }: TCheckboxProps) {
+  return (
+    <label className={[classes.container, className].filter(Boolean).join(' ')}>
+      <input {...props} type="checkbox" />
+      <span className={classes.checkmark}></span>
+      {label && <span className={classes.label}>{label}</span>}
+    </label>
+  );
+}
+
+export default Checkbox;
