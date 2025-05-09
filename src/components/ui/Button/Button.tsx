417fd64052a5a6306c6ca4b45f4b90966fc774c1
Alex-Nicalace <anicalace@gmail.com>
2025-05-09 10:15:47 +0300

    Button: create

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import type { ButtonHTMLAttributes } from 'react';
+import classes from './Button.module.css';
+
+type TButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {};
+function Button({ children, className, ...props }: TButtonProps) {
+  return (
+    <button
+      {...props}
+      className={[classes.container, className].filter(Boolean).join(' ')}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
